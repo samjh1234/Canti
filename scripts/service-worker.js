5dd335f606b281dad0834e2f4d515041c1630439
samjh1234 <wisamjh@hotmail.com>
2024-12-15 10:32:08 +0100

    Update service-worker.js

--- scripts/service-worker.js
+++ scripts/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "lyrics-pwa-cache-v7"; // Update version to invalidate old caches
+const CACHE_NAME = "lyrics-pwa-cache-v8"; // Update version to invalidate old caches
 
 const urlsToCache = [ 
   "https://samjh1234.github.io/canti/index.html",
